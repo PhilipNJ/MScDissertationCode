160d23852e8e0d4d8649b558124940130b7fa478
Philip NJ <philipnj96@gmail.com>
2024-07-06 20:14:19 +0100

    updated filenames, relevant code and evidenced changes

--- Data_extraction.py
+++ Data_extraction.py
@@ -4,11 +4,11 @@ end_date = '2024-03-31'
 start_date = '2004-03-31'
 
 
-ticker_list = ['^GSPC','^NSEI',"^FTSE"]
+ticker_dict = { 'SP500': '^GSPC', 'NIFTY50': '^NSEI', 'FTSE100': '^FTSE'}
 
-for ticker in ticker_list:
+for ticker in ticker_dict:
     print('Downloading data for:',ticker)
-    data = yf.download(ticker, start=start_date, end=end_date)
+    data = yf.download(ticker_dict[ticker], start=start_date, end=end_date)
     print('Data Downloaded for:',ticker)
     name = str(ticker)
     data.to_pickle(f'data/{name}.pkl')
